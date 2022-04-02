e6948aec453b10f1d7b581708a6c54e41a9d4bd3
DIVYA-PIDUGU <95955816+PIDUGU-DIVYA@users.noreply.github.com>
2022-04-02 12:16:41 +0530

    product.css changed

--- urban-ladder/src/components/productpage/title.jsx
+++ urban-ladder/src/components/productpage/title.jsx
@@ -2,8 +2,8 @@ import "./product.css"
 export const Title_div = () => {
     return <>
         <div>
-            <h1 id="title">Beds With Storage</h1>
-            <p id="no_of_products">(20 Products)</p>
+            <h1 id="ptitle">Beds With Storage</h1>
+            <p id="pno_of_products">(20 Products)</p>
         </div>
     </> 
 }
